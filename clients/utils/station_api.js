d49a7e5d6c2b6af33a164463f729909876aa76dd
jemma <jemma_clark1@hotmail.com>
2023-10-17 14:04:05 +1100

    updated total stations stats

--- clients/utils/station_api.js
+++ clients/utils/station_api.js
@@ -1,7 +1,13 @@
-
 function fetchOwners() {
-    return fetch('/api/owners')
-        .then(res => res.json())
-        .then(res => {return res.length})
+  return fetch("/api/owners")
+    .then((res) => res.json())
+    .then((res) => {
+      return res.length;
+    });
 }
 
+function fetchStations() {
+  return fetch("/api/stations/all")
+    .then((res) => res.json())
+    .then((res) => res.length);
+}
