a20f8ec85f211d19af814b8daf23019d7575f68b
Wen-Hao Chu <leochu111@gmail.com>
2023-10-17 15:29:36 +1100

    add petrol station info

---
+++ clients/components/nearest_section.js
@@ -0,0 +1,14 @@
+fetchStations()
+    .then(res => {
+        for(i = 0; i < 10; i++) {
+            const HTML = `
+	        <p>${res[i].name}<strong></strong></br>${res[i].address}</p>
+            `
+        let stationDiv = document.createElement('div')
+        stationDiv.innerHTML = HTML;
+
+        const nearest = document.querySelector('.nearest')
+        nearest.appendChild(stationDiv);
+        }
+    })
+
--- clients/components/stats_section.js
+++ clients/components/stats_section.js
@@ -2,4 +2,6 @@ const totalOwners = document.querySelector(".owners");
 const totalStations = document.querySelector(".stations");
 
 fetchOwners().then((res) => (totalOwners.innerHTML = res));
-fetchStations().then((res) => (totalStations.innerHTML = res));
+fetchStations()
+    .then(res => res.length)
+    .then(res => (totalStations.innerHTML = res));
