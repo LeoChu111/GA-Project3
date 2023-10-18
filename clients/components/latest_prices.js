c4d9e29270ea2588fd21f6f5c984de3839c2f633
Taymen Verschuren <Taymen@iinet.net.au>
2023-10-18 17:56:18 +0800

    Nearest function working

--- clients/components/latest_prices.js
+++ clients/components/latest_prices.js
@@ -1,37 +1,37 @@
-const apiKey = "DJetyvmCGk8sgJSdfsDQs4JZtf3I40Ez4Q23jjao";
+const apiKey = "NEED NEW API KEY";
 const wtiPriceSpan = document.getElementById("wti-price");
 const brentPriceSpan = document.getElementById("brent-price");
 const naturalGasPriceSpan = document.getElementById("natural-gas-price");
 
-fetch(`https://api.futures-api.com/last?symbol=CL`, {
-  method: "GET",
-  headers: {
-    "x-api-key": apiKey,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    wtiPriceSpan.innerText = data.data[0].last;
-  });
+// fetch(`https://api.futures-api.com/last?symbol=CL`, {
+//   method: "GET",
+//   headers: {
+//     "x-api-key": apiKey,
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     wtiPriceSpan.innerText = data.data[0].last;
+//   });
 
-fetch(`https://api.futures-api.com/last?symbol=BB`, {
-  method: "GET",
-  headers: {
-    "x-api-key": apiKey,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    brentPriceSpan.innerText = data.data[0].last;
-  });
+// fetch(`https://api.futures-api.com/last?symbol=BB`, {
+//   method: "GET",
+//   headers: {
+//     "x-api-key": apiKey,
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     brentPriceSpan.innerText = data.data[0].last;
+//   });
 
-fetch(`https://api.futures-api.com/last?symbol=NG`, {
-  method: "GET",
-  headers: {
-    "x-api-key": apiKey,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    naturalGasPriceSpan.innerText = data.data[0].last;
-  });
+// fetch(`https://api.futures-api.com/last?symbol=NG`, {
+//   method: "GET",
+//   headers: {
+//     "x-api-key": apiKey,
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     naturalGasPriceSpan.innerText = data.data[0].last;
+//   });
--- clients/components/nearest_section.js
+++ clients/components/nearest_section.js
@@ -1,15 +1,15 @@
-fetchStations()
-    .then(res => {
-        for(i = 0; i <= 10; i++) {
-            const HTML = `
-	        <p>${res[i].name} <strong>1018m</strong></br>${res[i].address}</p>
-            `
-        let stationDiv = document.createElement('div')
-        stationDiv.innerHTML = HTML;
-        stationDiv.className = 'nearest-stations'
+// fetchStations().then((res) => {
+//   for (i = 0; i < 10; i++) {
+//     const HTML = `
+// 	        <p>${res[i].name} <strong>1018m</strong></br>${res[i].address}</p>
+//             `;
+//     let stationDiv = document.createElement("div");
+//     stationDiv.innerHTML = HTML;
+//     stationDiv.className = "nearest-stations";
 
-        const nearest = document.querySelector('.nearest')
-        nearest.appendChild(stationDiv);
-        }
-    })
+//     const nearest = document.querySelector(".nearest");
+//     nearest.appendChild(stationDiv);
+//   }
+// });
 
+// fetchClosestTen();
