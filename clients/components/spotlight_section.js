2303185d76c2376a640e924911928b7a679b79fc
Wen-Hao Chu <leochu111@gmail.com>
2023-10-18 17:26:34 +1100

    add logo to spotlight section

--- clients/components/spotlight_section.js
+++ clients/components/spotlight_section.js
@@ -5,10 +5,22 @@ const refreshLink = document.querySelector("#refresh-link");
 fetchRandomStation().then((res) => {
   randomName.innerText = res.name;
   randomAddress.innerText = res.address;
-
   let randomLat = Number(res.latitude);
   let randomLong = Number(res.longitude);
-
+  const image = {
+    Shell:"/images/shell.png",
+    BP: "/images/BP-logo.png",
+    "7-Eleven Pty Ltd": "/images/7eleven.png", 
+    "Independent Fuel Supplies": "/images/inde.png", 
+    Caltex: "/images/caltex.png",
+    GoogleMark: "/images/googlemapmarker.png"
+  }
+  let logo = document.querySelector('#spotlight-logo')
+  if (image.hasOwnProperty(res.owner)) {
+    logo.src = `${image[res.owner]}`
+  } else {
+    logo.src = `${image.GoogleMark}`
+  }
   randomName.addEventListener("click", handleRandomName);
 
   function handleRandomName(event) {
